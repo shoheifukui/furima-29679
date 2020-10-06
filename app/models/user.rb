668afaa58d45b71414125134f412facfeba9b3ca
shohei.fukui <doghu04@gmail.com>
2020-10-06 16:40:49 +0900

    ブラウザエラーメッセージ表示/バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-   with_options presence: true do
+  with_options presence: true do
    validates :nickname
    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-   validates :email, uniqueness: true, length: {minimum: 6}, format: { with: VALID_EMAIL_REGEX, message: "メールアドレスに@を含んでください。「email」内に「@」がありません"}
+   validates :email, uniqueness: true, length: {minimum: 6}, format: { with: VALID_EMAIL_REGEX}
    VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
    validates :password, format: {with: VALID_PASSWORD_REGEX, confirmation: true, with:/\A[a-zA-Z0-9]+\z/}
-   validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-   validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+   validates :family_name, format: { with: /\A[一-龥]/}
+   validates :first_name, format: { with: /\A[一-龥]/}
    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
    validates :birth_day
