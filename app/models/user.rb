f905fe6e53e02de9dc6dfe59427a361e0ae4b3a7
shohei.fukui <doghu04@gmail.com>
2020-10-06 19:08:13 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, uniqueness: true, length: {minimum: 6}, format: { with: VALID_EMAIL_REGEX}
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, format: {with: VALID_PASSWORD_REGEX, confirmation: true, with:/\A[a-zA-Z0-9]+\z/}
+
 
   with_options presence: true do
    validates :nickname
-   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-   validates :email, uniqueness: true, length: {minimum: 6}, format: { with: VALID_EMAIL_REGEX}
-   VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
-   validates :password, format: {with: VALID_PASSWORD_REGEX, confirmation: true, with:/\A[a-zA-Z0-9]+\z/}
-   validates :family_name, format: { with: /\A[一-龥]/}
-   validates :first_name, format: { with: /\A[一-龥]/}
-   validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
-   validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
    validates :birth_day
+   VALID_FULL_WIDTH_CHARACTOR_REGEX = /\A[一-龥]+\z/
+   validates :family_name, format: { with: VALID_FULL_WIDTH_CHARACTOR_REGEX, message: "Full-width characters"}
+   validates :first_name, format: { with: VALID_FULL_WIDTH_CHARACTOR_REGEX, message: "Full-width characters"}
+   VALID_FULL_WIDTH_KATAKANA_CHARACTOR_REGEX = /\A[ァ-ヶー－]+\z/ 
+   validates :family_name_kana, format: { with: VALID_FULL_WIDTH_KATAKANA_CHARACTOR_REGEX, message: "Full-width katakana characters"}
+   validates :first_name_kana, format: { with: VALID_FULL_WIDTH_KATAKANA_CHARACTOR_REGEX, message: " Full-width katakana characters"}
    end
 end
