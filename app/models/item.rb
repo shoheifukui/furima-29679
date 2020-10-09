df60f609c9a961a9b247f29cba1247f0250a9e99
shohei.fukui <doghu04@gmail.com>
2020-10-09 11:40:59 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,14 +4,15 @@ class Item < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-   with_options presence: true do
-    validates: name, length: {maximum: 40}
-    validates: detail, length: {maximum: 1000}
-    validates: category_id
-    validates: status_id
-    validates: price, length: {minimum: 300, maximum: 9999999}
-    validates: delivery_fee_id
-    validates: region_id
-    validates: delivery_days_id
-    validates: user
+  with_options presence: true do
+  validates :name, length: {maximum: 40}
+  validates :detail, length: {maximum: 1000}
+  validates :category_id
+  validates :status_id
+  validates :price, length: {minimum: 300, maximum: 9999999}
+  validates :delivery_fee_id
+  validates :region_id
+  validates :delivery_days_id
+  validates :user
+  end
 end
