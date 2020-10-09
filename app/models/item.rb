ef25488260c1cbc7de765533f75b44d96efed0b1
shohei.fukui <doghu04@gmail.com>
2020-10-09 11:18:42 +0900

    itemモデルバリデーション設定

---
+++ app/models/item.rb
@@ -0,0 +1,17 @@
+class Item < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+   with_options presence: true do
+    validates: name, length: {maximum: 40}
+    validates: detail, length: {maximum: 1000}
+    validates: category_id
+    validates: status_id
+    validates: price, length: {minimum: 300, maximum: 9999999}
+    validates: delivery_fee_id
+    validates: region_id
+    validates: delivery_days_id
+    validates: user
+end
