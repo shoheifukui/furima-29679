a10f3830fbed6aebf20184891ea7c784b9cd747c
shohei.fukui <doghu04@gmail.com>
2020-10-09 12:45:53 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+    validates :name, length: {maximum: 40}
+    validates :detail, length: {maximum: 1000}
+    validates :category_id
+    validates :status_id
+    validates :price, length: {minimum: 300, maximum: 9999999}
+    validates :delivery_fee_id
+    validates :region_id
+    validates :delivery_days_id
+    validates :user
+  end
 end
