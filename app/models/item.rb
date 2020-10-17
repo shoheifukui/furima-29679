fcbe00ef60a263dea7b64a4c5f58915eaec19fed
shohei.fukui <doghu04@gmail.com>
2020-10-17 18:21:51 +0900

    エラーハンドリング設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,15 +4,25 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
+    validates :image
     validates :name, length: {maximum: 40}
     validates :detail, length: {maximum: 1000}
-    validates :category_id
-    validates :status_id
-    validates :price, numericality: {:greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999 }, format: { with: /\A[0-9]+\z/ }
-    validates :delivery_fee_id
-    validates :prefecture_id
-    validates :delivery_days_id
+    validates :price, format: {with: /\A[0-9]+\z/, message: "Half-width number"}, numericality: {:greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999, message: "Out of setting range"}
     validates :user
-    validates :image
   end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to_active_hash :category
+    belongs_to_active_hash :status
+    belongs_to_active_hash :delivery_fee
+    belongs_to_active_hash :prefecture
+    belongs_to_active_hash :delivery_day
+
+    with_options presence: true do
+      validates :category_id, inclusion: {in: 1..10, message: "Select"}
+      validates :status_id, inclusion: {in: 1..6, message: "Select"}
+      validates :delivery_fee_id, inclusion: {in: 1..2, message: "Select"}
+      validates :prefecture_id, inclusion: {in: 1..47, message: "Select"}
+      validates :delivery_days_id, inclusion: {in: 1..3, message: "Select"}
+    end
 end
