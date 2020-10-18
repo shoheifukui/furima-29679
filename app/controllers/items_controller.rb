1277b3993ebedb2a1c6154f277ee60faf5f6659a
shohei.fukui <doghu04@gmail.com>
2020-10-18 21:40:15 +0900

    Revert "商品情報編集機能削除"
    This reverts commit 25b252e0bb3b77a1e1aa26b735d960c36aca3ebf.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,9 +14,22 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path(@item)
+      redirect_to root_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
     end
   end
 
