ee368caf9088ea21e9fd8614c077ad0cdbd07e6c
shohei.fukui <doghu04@gmail.com>
2020-10-19 10:58:26 +0900

    コード修正３回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,7 +28,6 @@ before_action :move_to_index, except: [:index, :show, :new, :create]
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
